b679838e7211829aa5868cbcc9604d136da1e2dc
jreleaserbot <jreleaser@kordamp.org>
2024-01-12 07:55:30 +0000

    tetris-tui v0.2.10

--- Formula/tetris-tui.rb
+++ Formula/tetris-tui.rb
@@ -1,26 +1,26 @@
-# Generated with JReleaser 1.10.0 at 2024-01-12T01:24:45.813102092Z
+# Generated with JReleaser 1.10.0 at 2024-01-12T07:55:21.491389165Z
 
 class TetrisTui < Formula
   desc "Play tetris in the terminal"
   homepage "https://github.com/quantonganh/tetris-tui/"
-  version "0.2.2"
+  version "0.2.10"
   license "MIT"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/quantonganh/tetris-tui/releases/download/v0.2.2/tetris-tui-0.2.2-aarch64-unknown-linux-gnu.zip"
-    sha256 "9161bac92c4faf703225903ecaf1bd32308794db1e82a37e3fa8d0197e14ca6b"
+    url "https://github.com/quantonganh/tetris-tui/releases/download/v0.2.10/tetris-tui-0.2.10-aarch64-unknown-linux-gnu.zip"
+    sha256 "d42402f1da96d104fe211b1de895e661153dda0edd148fff22fc110d2fb83951"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/quantonganh/tetris-tui/releases/download/v0.2.2/tetris-tui-0.2.2-x86_64-unknown-linux-gnu.zip"
-    sha256 "d42dda3b37a701231fb9aa69593f25903b204f74cdaeb7aea7c9ff83d190b898"
+    url "https://github.com/quantonganh/tetris-tui/releases/download/v0.2.10/tetris-tui-0.2.10-x86_64-unknown-linux-gnu.zip"
+    sha256 "e9fccee2e76514e3602098377694eb34b221ca495f3124097fc5a9d0f1d98843"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/quantonganh/tetris-tui/releases/download/v0.2.2/tetris-tui-0.2.2-aarch64-apple-darwin.zip"
-    sha256 "a0dbd7950dff7a2861471be52a32caa7467711f00b6a53cfff8b6d2e388ff212"
+    url "https://github.com/quantonganh/tetris-tui/releases/download/v0.2.10/tetris-tui-0.2.10-aarch64-apple-darwin.zip"
+    sha256 "07482d3a0c15b3d18f82bfd9a57452e1e017e31250c06749bc2f6462d2ed4e11"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/quantonganh/tetris-tui/releases/download/v0.2.2/tetris-tui-0.2.2-x86_64-apple-darwin.zip"
-    sha256 "0b10d74fe59406fd871d01ce50b374dd3a38e702ff698a0340d335fc6ee7d42d"
+    url "https://github.com/quantonganh/tetris-tui/releases/download/v0.2.10/tetris-tui-0.2.10-x86_64-apple-darwin.zip"
+    sha256 "d29541dcf7281061ac41813418d802cde1a102f7050ebbba7bc03e56481a0c3b"
   end
 
 
@@ -31,6 +31,6 @@ class TetrisTui < Formula
 
   test do
     output = shell_output("#{bin}/tetris-tui --version")
-    assert_match "0.2.2", output
+    assert_match "0.2.10", output
   end
 end
